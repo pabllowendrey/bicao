a08209c5346935c4d704eb4ccab572a849cee364
PablloW <pabllowendrey@gmail.com>
2026-04-15 08:31:31 -0300

    Add colunm image in products

---
+++ db/migrate/20260414114933_img_product.rb
@@ -0,0 +1,5 @@
+class ImgProduct < ActiveRecord::Migration[5.2]
+  def change
+    add_column :products, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_04_09_012131) do
+ActiveRecord::Schema.define(version: 2026_04_14_114933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2026_04_09_012131) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "price"
+    t.string "image"
     t.index ["type_product_id"], name: "index_products_on_type_product_id"
   end
 
